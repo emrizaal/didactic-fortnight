5a30b7817d5157dd1c27aedb99939da84eeafa25
DESKTOP-DETT4F3\Novia <sukmasarin@gmail.com>
2016-11-13 17:50:19 +0700

    + desain login

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,19 +1,42 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-  </head>
-  <body>
-    <?php
-      if(isset($error) && $error) {
-        echo "Error";
-      }
-    ?>
-    <?php echo form_open('admin/login_admin'); ?>
-      <input type="text" name="username" placeholder="Masukkan Username" required>
-      <input type="password" name="password" placeholder="Masukkan Password" required>
-      <input type="submit" name="submit" value="Login">
-    <?php echo form_close(); ?>
+<?php include("/../header.php"); ?>
+  <body class="bglogin">
+	<div class="container" style="padding-top:10%">
+		<div id="myModals" class="modal-fade">
+			<div class="modal-dialog" style="width:33%">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h4>Login Admin</h4>
+					</div>
+					<div class="modal-body">
+						<?php echo form_open('admin/login_admin'); 
+						 if(isset($error) && $error) {
+							echo "<p class='bg-danger text-danger' style='padding:10px; border-radius:4px'>Username dan Password yang Anda masukkan salah!</p><br>";
+						  }
+						?>
+						
+						<form class="form-inline">
+						  <div class="form-group">
+							<div class="input-group">
+							  <div class="input-group-addon"><i class="fa fa-user-circle-o" aria-hidden="true" title="Username"></i></div>
+							  <input type="text" name="username" placeholder="Username" required class="form-control">
+							</div>
+						  </div>
+						  <div class="form-group">
+							<div class="input-group">
+							  <div class="input-group-addon"><i class="fa fa-key" aria-hidden="true" title="Password"></i></div>
+							  <input type="password" name="password" placeholder="Password" required class="form-control">
+							</div>
+						  </div>
+						<div style="text-align: right">
+							<input type="submit" name="submit" value="Login" class="btn btn-primary">
+							<?php echo form_close(); ?>
+						</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    
   </body>
 </html>
